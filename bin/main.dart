442dfb0bf45598250da34d9b5b0b8d70696d3cd9
Priyanshu Goyal <147061762+Priyanshu8839@users.noreply.github.com>
2025-01-02 22:30:13 +0530

    Dart Learning Day-1

---
+++ bin/main.dart
@@ -0,0 +1,18 @@
+void main() {
+
+  // This is a Single line comment
+
+  /*
+  This is a multi-line comment
+  */
+
+  // This is Print Satement
+  print("Hello' Dart");
+
+  // Variable Name & Rules
+  var name = "Priyanshu Goyal";
+  // Print a variable
+  print(name);
+
+  
+}
