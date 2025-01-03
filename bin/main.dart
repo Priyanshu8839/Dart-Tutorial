760dab86ecbe4de7f4e1c28a388f2912b029ab5a
Priyanshu Goyal <147061762+Priyanshu8839@users.noreply.github.com>
2025-01-03 22:12:31 +0530

    Dart Learning Day-2

--- bin/main.dart
+++ bin/main.dart
@@ -14,5 +14,30 @@ void main() {
   // Print a variable
   print(name);
 
+   // Built -in Data Types
+  // 1. Integers
+  int cart_item = 10;
+  print(cart_item);
+
+  // 2. Double
+  double price = 10.99;
+  print(price);
+
+  // 3. String
+  String city = "Delhi";
+  print(city);
+
+  // 4. boolian
+  bool is_admin = true;
+  print(is_admin);
+
+  bool is_login = false;
+  print(is_login);
+
+  var quantity = "100kg";
+  print(quantity);
+
+  // Check datatype
+  print(quantity.runtimeType);
   
 }
