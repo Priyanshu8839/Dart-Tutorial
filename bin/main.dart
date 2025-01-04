530e31090c419e134566fed544fc42892cd23f97
Priyanshu8839 <Priyanshugoyal8839@gmail.com>
2025-01-04 22:15:51 +0530

    Dart Learning Day-3

--- bin/main.dart
+++ bin/main.dart
@@ -40,4 +40,50 @@ void main() {
   // Check datatype
   print(quantity.runtimeType);
   
+  // Opeartors
+// 1. Arithmetic Operators
+  var a = 10;
+  var b = 20;
+  var c = 50;
+  var d = 100;
+  print(a + b);
+  print(a - c);
+  print(a * d);
+  print(d / b);
+  print(d % b);
+
+  //Prefix and Postfix Increment and Decrement operators
+  var i = 3;
+  print(i);
+  print(i++);
+  print(i);
+
+  // Equality and Relational operators
+  var x = 100;
+  var y = 200;
+  print(x == y);
+  print(x != y);
+  print(x > y);
+  print(x < y);
+  print(x >= y);
+  print(x <= y);
+
+  // Logical Operators
+  var p = 100;
+  var m = 100;
+  var q = 200;
+  var o = 200;
+  print((p == m) && (q == o));
+  print((p == m) || (q == o));
+
+  // Type test Operators
+  var nam = "Sonam";
+  var numbe = 100;
+  print(nam is String);
+  print(numbe is! int);
+
+  // Assignment Operators
+  var n = 10;
+  n += 5; //n = n + 5
+  print(n);
 }
